8dd7fc8bb8993504acdb4f2f2c4eee5e11836614
ankita-shah1234 <ankitashah2025@gmail.com>
2025-03-19 22:46:27 +0530

    add it too

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import './index.css'
-import App from './App.jsx'
-import { store } from "./redux/store/store.js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import App from "./App.jsx";
+import "./index.css";
+import { store } from "./redux/store/store.js";
+//! instance of react query
+const client = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <Provider store={store}>
-    <App />  
+      <QueryClientProvider client={client}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
-    
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
